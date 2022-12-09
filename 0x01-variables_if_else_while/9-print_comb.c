3d51e049b1c70cfa77e2bef75617abcbeee56bde
Feyi12 <fendebu@gmail.com>
2022-12-09 12:08:04 -0800

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+		int c = 0;
+
+		while (c < 10)
+		{
+			putchar(48 + c);
+			if (c != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}										c++;
+										  		}
+		putchar('\n');
+		return (0);
+
+}
+
+Footer
+
+© 2022 GitHub, In
