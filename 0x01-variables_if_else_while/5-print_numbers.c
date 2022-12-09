fec5e4a2b8efe519c0396f92ee0aa12e15594cb2
Feyi12 <fendebu@gmail.com>
2022-12-09 11:05:40 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		putchar(0);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
+
