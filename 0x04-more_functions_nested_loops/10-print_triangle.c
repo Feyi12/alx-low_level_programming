d18aa5aca4d675274e67cbb1334cd99e0ba2f1ba
Feyi12 <fendebu@gmail.com>
2022-12-16 06:01:44 -0800

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_triangle
+ * @size : integer type
+ * Return : void
+ */
+void print_triangle(int size)
+{
+
+	int i, ii;
+
+	while (i <= size && size > 0)
+	{
+		ii = 0;
+		while (ii < size - i)
+		{
+			_putchar (' ');
+			ii++;
+		}
+		ii = 0;
+	        
+		while (ii < i)
+		{
+			_putchar ('#');
+			ii++;
+		}
+
+		_putchar ('\n');
+		ii++;
+
+	}
+	if (i == 1)
+	_putchar ('\n');
+}
+
