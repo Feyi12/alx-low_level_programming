8a9fa6bd9d2f3c33372571a8d19681c2652b2b96
Feyi12 <fendebu@gmail.com>
2022-12-19 08:53:43 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,11 +9,13 @@ void print_rev(char *s)
 {
 	int counter = 0;
 	int i, n;
+
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		counter++;
 	}
-	for (n= (counter - 1); n >= 0; n--)
+	for (n = (counter - 1); n >= 0; n--)
 	{
 		_putchar(s[n]);
 	}
