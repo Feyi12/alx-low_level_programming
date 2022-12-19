6c0bab4c4323cb3fa076c61be9ce0b8460009269
Feyi12 <fendebu@gmail.com>
2022-12-18 20:47:14 -0800

    main.h

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"                                                                                                                                                                               
+/**                                                                                                                                                                                            
+ * print_alphabet_x10 - alphabet in lowercase letter that 10x                                                                                                                                
+ * description: print lowercase alphabet                                                                                                                                                        
+ *
+ * Return: Always 0                                                                                                                                                                              */
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char lletter;
+	while (count < 10)
+	{
+		for (lletter = 'a'; lletter <= 'z'; lletter++)
+		{
+			_putchar(lletter);
+		}
+		_putchar('\n');
+	}
+}
