fc00d528a0b9ce188daae2864773123c6be0c7c4
Feyi12 <fendebu@gmail.com>
2022-12-19 05:29:59 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  _strlen - af function that return a length of string
+ *  @s: character input
+ *  Return: length of input string
+ */
+
+int _strlen(char *s)
+{
+	int i = 1, len = 0;
+	char s1 = s[0];
+
+	while (sl != '\0')
+	{
+		len++;
+		s1 = s[++];
+	}
+	return (len);
+}
