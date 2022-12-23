1c3c75a8e6a3721e8137903cf1a130e354945de5
Feyi12 <fendebu@gmail.com>
2022-12-23 10:49:55 -0800

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * reverse_array - function that reverses the content of an array of integers
+ * @a: content of array
+ * @n: elements of array
+ * Return: Always 0
+ */
+void reverse_array(int *a, int n)
+{
+	int i, fen;
+
+	for (i = 0; i < n; i++)
+	{
+		n--;
+		fen = a[i];
+		a[i] = a[n];
+		a[n] = fen;
+	}
+	return (0);
+}
