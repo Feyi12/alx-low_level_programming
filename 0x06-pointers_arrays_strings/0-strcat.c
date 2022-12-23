7ba7dbdeaf16d1ec78b7d9c988d4250952c89825
Feyi12 <fendebu@gmail.com>
2022-12-23 09:46:25 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *_strcat - function that concatenate two strings
+ * @src: source of file
+ * @dest: destination of file
+ * Return: the concatenated strings
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int n;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+
+	}
+
+	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
+	{
+
+	}
+
+	return (dest);
+}
+
