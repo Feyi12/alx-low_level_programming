63918b43929fe301768a49212bc593244b57bd5c
Feyi12 <fendebu@gmail.com>
2023-01-05 09:01:44 -0800

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * primeof - Makes possible to evaluate from 1 to n
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
+ * Return: Always 0
+ */
+int primeof(int a, int b)
+{
+	if (a == b)
+	{
+		return (1);
+	}
+	else if (a % b == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (primeof(a, b + 1));
+	}
+}
+/**
+ * is_prime_number - function that returns 1 if the input integer is a prime number
+ * otherwise return 0
+ * @n: input integer
+ * Return: Always (success)
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (primeof(n, 2));
+	}
+}
