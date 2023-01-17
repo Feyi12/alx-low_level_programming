6006b84ceea2abbebc2f69b5732a794672722cf5
Feyi12 <fendebu@gmail.com>
2023-01-16 17:54:33 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,12 +10,13 @@
 int _strlen(char *str)
 {
 	int i;
-
+	
 	for (i = 0; str[i]; i++)
 		;
 	
 	return (i);
 }
+
 /**
  * _strcpy - Copies a string from src to dest
  *  @dest: String to be copied to
@@ -25,13 +26,14 @@ int _strlen(char *str)
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-
+	
 	for (i = 0; src[i]; i++)
 		dest[i] = src[i];
 	dest[i] = '\0';
-
+	
 	return (dest);
 }
+
 /**
  * new_dog - Program that creates a new dog. That is, creates a new struct
  *  @name: name char array
@@ -44,43 +46,43 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *newDog = NULL;
 	char *newName, *newOwner;
 	int newNameLen, newOwnerLen;
-
+	
 	if (name == NULL || owner == NULL)
 		return (NULL);
-
+	
 	newDog = malloc(sizeof(dog_t));
-
+	
 	if (newDog == NULL)
 		return (NULL);
-
+	
 	newNameLen = _strlen(name);
 	newName = malloc(sizeof(char *) * (newNameLen + 1));
-
+	
 	if (newName == NULL)
 	{
 		free(newDog);
 		return (NULL);
 	}
-
+	
 	newOwnerLen = _strlen(owner);
 	newOwner = malloc(sizeof(char *) * (newOwnerLen + 1));
-
+	
 	if (newName == NULL)
 	{
 		free(newDog);
 		return (NULL);
 	}
-
+	
 	newOwnerLen = _strlen(owner);
 	newOwner = malloc(sizeof(char *) * (newOwnerLen + 1));
-
+	
 	if (newOwner == NULL)
 	{
 		free(newName);
 		free(newDog);
 		return (NULL);
 	}
-
+	
 	newDog->name = _strcpy(newName, name);
 	newDog->age = age;
 	newDog->owner = _strcpy(newOwner, owner);
