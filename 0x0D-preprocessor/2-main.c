593dfd56bcc53bee0a38059eaa08193311d7ddaa
Feyi12 <fendebu@gmail.com>
2023-01-16 18:37:26 -0800

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints out the name of the file
+ * @void: Nothing
+ * Return: Success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
